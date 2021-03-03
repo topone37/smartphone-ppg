0bbabbd409abaa014ed17eeeb418c1f14a3a51de
lhosszu <levihosszu@gmail.com>
2021-03-03 20:26:23 +0100

    Initial commit

---
+++ app/src/main/java/com/uni/ppg/MainActivity.java
@@ -0,0 +1,20 @@
+package com.uni.ppg;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+}
